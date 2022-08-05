a18766935d78e04240b77d70862838303e962c3f
Andrea055 <vivanco11.ac@gmail.com>
2022-08-05 11:14:52 +0200

    fix script

--- build.sh
+++ build.sh
@@ -1,16 +1,15 @@
 dirname = Ableton_Live_11_Suite
 desktop = $(xdg-user-dir DESKTOP)
 echo "Preparing the folder..."
-cd ~/wol
-mkdir $dirname
+mkdir ~/wol/Ableton_Live_11_Suite
 echo "Installing all dependencies for Ableton Live 11 Suite and preparing all enviroments, Please wait..."
-WINEARCH=win64 WINEPREFIX=~/wol/$dirname winetricks d3dx9 dotnet35 dotnet452 dxvk gdiplusvcrun2019 corefonts tahoma vcrun6sp6
-WINEARCH=win64 WINEPREFIX=~/wol/$dirname winetricks win10
+WINEARCH=win64 WINEPREFIX=~/wol/Ableton_Live_11_Suite winetricks d3dx9 dotnet35 dotnet452 dxvk gdiplusvcrun2019 corefonts tahoma vcrun6sp6
+WINEARCH=win64 WINEPREFIX=~/wol/Ableton_Live_11_Suite winetricks win10
 echo "Creating shortcut on your desktop, please don't open it until the installer finish its work"
-cp ./ableton.sh ~/wol/$dirname
-cp ./icon.png ~/wol/$dirname
-cp ./ableton.desktop ~/$desktop
-cd ~/wol/$dirname/drive_c
+cp ~/wol/Downloads/Ableton-On-Linux/ableton.sh ~/wol/Ableton_Live_11_Suite
+cp ~/wol/Downloads/Ableton-On-Linux/icon.png ~/wol/Ableton_Live_11_Suite
+cp ~/wol/Downloads/Ableton-On-Linux/ableton.desktop ~/$desktop
+cd ~/wol/Ableton_Live_11_Suite/drive_c
 echo "Downloading Ableton Live 11 Suite, this depends on your internet connection speed, Please wait..."
 wget https://cdn-downloads.ableton.com/channels/11.1.5/ableton_live_suite_11.1.5_64.zip
 echo "Unzipping Ableton Live 11 Suite..."
